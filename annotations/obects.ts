b300c84cd9286640731ad36c9035a782ed3f25ca
Jennifer Udensi <okosisijenifer@gmail.com>
2023-08-07 22:47:50 -0300

    type annotation

---
+++ annotations/obects.ts
@@ -0,0 +1,22 @@
+const profile = {
+  name: 'alex',
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+    this.age = age;
+  }
+
+};
+
+const { age }: { age: number } = profile;
+
+//for plain destructuring
+//const { coords: { lat, lng} } = profile;
+
+//for annotation destructuring
+const {
+   coords: { lat, lng }
+   }: { coords: { lat: number; lng: number } } = profile;
---
+++ tuples.ts
@@ -0,0 +1,17 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40
+};
+
+//tuple
+type Drink = [string, boolean, number]
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['yellow', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+};
